acb215a80dfa41d303f61ead70014573357876d2
knoff <nixos-git@knoc.one>
2023-04-11 00:32:52 +0200

    Added impermanence to conversations
    right now its at 1 minute, but ill change to 30 minutes or so.
    need to allow jeoff to respond if the message doesent contain his name
    and if the message is sent within 2 minutes or less, and the same
    channel that it was previously in.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
+use chatgpt::prelude::*;
+use chrono::{Duration, Utc};
 use serenity::{
     async_trait,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
-
-use chrono::{Utc, Duration};
-
-use chatgpt::prelude::*;
-
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
-static MY_STRING: &str = "
-";
-
 // Define a handler struct
 struct Handler {
     chat_gpt_client: ChatGPT,
-    conversations: Arc<Mutex<HashMap<u64, Conversation>>>,
-    //conversation: Arc<Mutex<Conversation>>,
+    conversations: Arc<Mutex<HashMap<u64, (Conversation, chrono::DateTime<Utc>)>>>,
+    // conversations: Arc<Mutex<HashMap<u64, Conversation>>>,
+    // conversation: Arc<Mutex<Conversation>>,
 }
-
 
 impl Handler {
     async fn new_chatbot(client: ChatGPT) -> Self {
@@ -32,16 +26,38 @@ impl Handler {
     }
 
     async fn chatbot(&self, channel_id: u64, input_str: &String) -> Result<String> {
+        // Lock the conversations HashMap
         let mut conversations = self.conversations.lock().await;
-        let conversation = conversations
-            .entry(channel_id)
-            .or_insert_with(|| self.chat_gpt_client.new_conversation());
 
-        let response = conversation.send_message(input_str).await?;
+        // Get the current time
+        let now = Utc::now();
+
+        // Attempt to find an existing conversation for the given channel_id
+        // If it doesn't exist, create a new conversation and store the current timestamp as the last message time
+        let conversation_entry = conversations.entry(channel_id).or_insert_with(|| {
+            println!("Generating a new conversation for channel {}", channel_id);
+            (self.chat_gpt_client.new_conversation(), Utc::now())
+        });
+
+        // Check if the conversation's last message time is older than 10 minutes
+        // If it is, recreate the conversation and update the last message time to the current time
+        if now.signed_duration_since(conversation_entry.1) > Duration::minutes(1) {
+            println!("Refreshing the conversation for channel {}", channel_id);
+            *conversation_entry = (self.chat_gpt_client.new_conversation(), Utc::now());
+        } else {
+            println!("Using an existing conversation for channel {}", channel_id);
+        }
+
+        // Send the user's message to the conversation and receive a response
+        let response = conversation_entry.0.send_message(input_str).await?;
+
+        // Update the conversation's last message time to the current time
+        conversation_entry.1 = Utc::now();
+
+        // Return the response content as a String
         Ok(response.message().content.to_string())
     }
 }
-
 
 // Implement EventHandler trait for the Handler struct
 #[async_trait]
@@ -67,7 +83,6 @@ impl EventHandler for Handler {
             .to_lowercase()
             .contains(&bot_user.name.to_lowercase())
         {
-
             // Concatenate the messages to form the context string
             // let mut context = MY_STRING.to_owned();
             // println!("Context: {}", context );
@@ -75,19 +90,21 @@ impl EventHandler for Handler {
             // Reply to the message with a simple text
             let _ = msg
                 .channel_id
-                .say(&ctx.http, self.chatbot(msg.channel_id.0, &msg.content).await.unwrap())
+                .say(
+                    &ctx.http,
+                    self.chatbot(msg.channel_id.0, &msg.content).await.unwrap(),
+                )
                 .await;
         }
     }
 }
 
-
-
 #[tokio::main]
 async fn main() {
     // Read the bot token from an environment variable
     let token = std::env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    let chat_gpt_key = std::env::var("OPENAI_API_KEY").expect("Expected a token in the environment");
+    let chat_gpt_key =
+        std::env::var("OPENAI_API_KEY").expect("Expected a token in the environment");
     let client = ChatGPT::new(chat_gpt_key).unwrap();
 
     let handler = Handler::new_chatbot(client).await;
