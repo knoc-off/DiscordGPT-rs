8431456256db25551c40d920237a069416d1f840
knoff <nixos-git@knoc.one>
2023-04-13 14:55:13 +0200

    Added some basic error handling
    split the large chatbot funciton into smaller bits.

--- src/handler.rs
+++ src/handler.rs
@@ -1,9 +1,9 @@
 use chatgpt::prelude::*;
+
 use chrono::{Duration, Utc};
 use serenity::prelude::*;
 use std::{collections::HashMap, sync::Arc};
 use tokio::{sync::mpsc, sync::Mutex};
-
 
 use serenity::model::prelude::ChannelId;
 
@@ -52,22 +52,38 @@ impl Handler {
             };
 
             if let Some(queued_message) = queued_message {
-                //let msg = &queued_message.message;
-                let response = self
+                let response_result = self
                     .chatbot(
                         queued_message.channel_id,
                         &(queued_message.author_name + ": " + &queued_message.content),
                     )
-                    .await
-                    .unwrap(); // This Errors when the token length is exceeded
-
-                println!("Response: {}", response);
-
-                let _ = ChannelId(queued_message.channel_id)
-                    .say(&ctx.http, response)
                     .await;
-            }
 
+                match response_result {
+                    Ok(response) => {
+                        println!("Response: {}", response);
+                        let _ = ChannelId(queued_message.channel_id)
+                            .send_message(&ctx.http, |m| {
+                                m.content(response);
+                                m.tts(true)
+                            })
+                            .await;
+                    }
+                    Err(e) => {
+                        eprintln!("Error: {}", e);
+
+                        let mut conversations = self.conversations.lock().await;
+
+                        println!("Attempting to Reset");
+
+                        if let Some(conversation_entry) =
+                            conversations.get_mut(&queued_message.channel_id)
+                        {
+                            self.handle_reset(conversation_entry)
+                        }
+                    }
+                }
+            }
             tokio::time::sleep(std::time::Duration::from_secs(3)).await;
         }
     }
@@ -76,7 +92,28 @@ impl Handler {
         // Lock the conversations HashMap
         let mut conversations = self.conversations.lock().await;
 
-        // Get the current time
+        let conversation_entry = self
+            .get_or_create_conversation(&mut conversations, channel_id, input_str)
+            .await;
+
+        self.handle_reset(conversation_entry);
+
+        // Send the user's message to the conversation and receive a response
+        let response = conversation_entry.0.send_message(input_str).await?;
+
+        // Update the conversation's last message time to the current time
+        conversation_entry.1 = Utc::now();
+
+        // Return the response content as a String
+        Ok(response.message().content.to_string())
+    }
+
+    async fn get_or_create_conversation<'a>(
+        &'a self,
+        conversations: &'a mut HashMap<u64, (Conversation, chrono::DateTime<Utc>)>,
+        channel_id: u64,
+        input_str: &String,
+    ) -> &mut (Conversation, chrono::DateTime<Utc>) {
         let now = Utc::now();
 
         // Attempt to find an existing conversation for the given channel_id
@@ -95,9 +132,7 @@ impl Handler {
 
         // Check if the conversation's last message time is older than 10 minutes
         // If it is, recreate the conversation with the chosen preset and update the last message time to the current time
-        if now.signed_duration_since(conversation_entry.1) > Duration::minutes(5)
-            || input_str.contains("!reset!")
-        {
+        if now.signed_duration_since(conversation_entry.1) > Duration::minutes(5) {
             let preset = get_preset_based_on_sentiment(input_str);
             println!(
                 "Refreshing the conversation for channel: {}, with preset: {}",
@@ -109,13 +144,32 @@ impl Handler {
             );
         }
 
-        // Send the user's message to the conversation and receive a response
-        let response = conversation_entry.0.send_message(input_str).await?;
+        conversation_entry
+    }
 
-        // Update the conversation's last message time to the current time
-        conversation_entry.1 = Utc::now();
+    fn handle_reset(&self, conversation_entry: &mut (Conversation, chrono::DateTime<Utc>)) {
+        // If the conversation history contains more than 20 messages, recreate the conversation with the last 10 messages and pre-prompt message
+        if conversation_entry.0.history.len() > 20 {
+            let pre_prompt_message = conversation_entry.0.history.get(0).cloned().unwrap();
+            let mut last_10_messages = conversation_entry
+                .0
+                .history
+                .iter()
+                .rev()
+                .take(10)
+                .cloned()
+                .collect::<Vec<_>>();
+            last_10_messages.insert(0, pre_prompt_message);
 
-        // Return the response content as a String
-        Ok(response.message().content.to_string())
+            println!("Recreating the conversation with the following messages:");
+            for message in &last_10_messages {
+                println!("{message:#?}")
+            }
+
+            *conversation_entry = (
+                Conversation::new_with_history(self.chat_gpt_client.clone(), last_10_messages),
+                Utc::now(),
+            );
+        }
     }
 }
