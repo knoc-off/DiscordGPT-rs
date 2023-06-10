7dc54d7980351f459db6a313d4ccd12635d5b179
knoff <nixos-git@knoc.one>
2023-06-10 12:37:46 +0200

    Updated the flake, now builds binary

--- flake.nix
+++ flake.nix
@@ -3,24 +3,50 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
     fenixrs = {
-      #url = "github:nix-community/fenix";
       url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     devenv.url = "github:cachix/devenv";
   };
 
   outputs = { self, fenixrs, nixpkgs, devenv, ... }@inputs:
     let
-      fenix = import fenixrs { system = "x86_64-linux"; };
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in {
-      devShell.x86_64-linux = devenv.lib.mkShell {
+      appName = "discord-gpt";
+      version = "0.1.0";
 
+      system = "x86_64-linux";
+
+      fenix = import fenixrs { inherit system; };
+      pkgs = import nixpkgs { inherit system; };
+
+      rustPkgs = with fenix; combine (with default; [
+        cargo
+        clippy-preview
+        latest.rust-src
+        rust-analyzer
+        rust-std
+        rustc
+        rustfmt-preview
+      ]);
+
+      packages = with pkgs; [
+        rustPkgs
+        cargo-edit
+        cargo-watch
+        mold
+        lld
+        clang
+        fuse
+        pkgconfig
+        openssl
+      ];
+    in
+    {
+      devShell.${system} = devenv.lib.mkShell {
         inherit inputs pkgs;
         modules = [
           ({ pkgs, ... }: {
-
             enterShell = ''
               export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
                 pkgs.lib.makeLibraryPath [
@@ -28,30 +54,30 @@
                 ]
               }"
             '';
-
-            packages = with pkgs; [
-              (with fenix;
-                combine (with default; [
-                  cargo
-                  clippy-preview
-                  latest.rust-src
-                  rust-analyzer
-                  rust-std
-                  rustc
-                  rustfmt-preview
-                ]))
-              cargo-edit
-              cargo-watch
-              mold
-              lld
-              clang
-              fuse
-
-              pkgconfig
-              openssl
-            ];
+            packages = packages;
           })
         ];
+      };
+
+      packages.${system}.app = (pkgs.makeRustPlatform {
+        cargo = fenix.minimal.cargo;
+        rustc = fenix.minimal.rustc;
+      }).buildRustPackage {
+        pname = appName;
+        version = version; # replace this with your actual version number
+
+        src = ./.; # replace this with the path to your source code
+
+        cargoLock.lockFile = ./Cargo.lock; # replace this with the path to your Cargo.lock file
+
+        nativeBuildInputs = packages;
+
+        #OPENSSL_DIR = "${pkgs.openssl.dev}";
+
+        buildInputs = with pkgs; [
+          openssl
+        ];
+
       };
     };
 }
